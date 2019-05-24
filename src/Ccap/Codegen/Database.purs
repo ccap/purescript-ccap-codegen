2b9d0f240abf0644a30275d739da6a6d50825c80
Peter Brant <peter.brant@gmail.com>
2019-05-24 19:20:12 +0200

    More discussion fodder

--- src/Ccap/Codegen/Database.purs
+++ src/Ccap/Codegen/Database.purs
@@ -1,17 +1,19 @@
 module Ccap.Codegen.Database
   ( domainModule
   , poolConfiguration
+  , tableModule
   ) where
 
 import Prelude
 
-import Ccap.Codegen.Types (Module(..), Primitive(..), TopType(..), Type(..), TypeDecl(..))
+import Ccap.Codegen.Types (Module(..), Primitive(..), RecordProp(..), TopType(..), Type(..), TypeDecl(..))
 import Control.Monad.Except (ExceptT, withExceptT)
 import Data.Map (empty) as Map
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Database.PostgreSQL.PG (Pool, PoolConfiguration, Query(..), defaultPoolConfiguration, query, withConnection)
-import Database.PostgreSQL.Row (Row0(..), Row2(..))
+import Database.PostgreSQL.Row (Row0(..), Row1(..), Row2(..), Row4(..))
 import Effect.Aff (Aff)
+import Text.Parsing.Parser.Pos (Position(..))
 
 -- TODO should be configurable
 poolConfiguration :: PoolConfiguration
@@ -24,10 +26,47 @@ poolConfiguration = (defaultPoolConfiguration "cc")
 
 domainModule :: Pool -> ExceptT String Aff Module
 domainModule pool = withExceptT show $ withConnection pool \conn -> do
-  results <- query conn (Query domainsSql) Row0
+  results <- query conn (Query sql) Row0
   let types = results <#> (\(Row2 domainName dataType) ->
                 TypeDecl domainName (Wrap (Primitive (dbNameToPrimitive dataType)) Map.empty))
   pure $ Module "Domains" types
+  where
+    -- TODO Support other types (date/time types in particular)
+    sql = """
+          select domain_name, data_type
+          from information_schema.domains
+          where domain_schema = 'public' and
+                  data_type in ('numeric', 'character varying', 'integer', 'smallint', 'text', 'boolean')
+          """
+
+tableModule :: Pool -> String -> ExceptT String Aff Module
+tableModule pool tableName = withExceptT show $ withConnection pool \conn -> do
+  results <- query conn (Query sql) (Row1 tableName)
+  let props = results <#> (\(Row4 columnName dataType domainName isNullable) ->
+                              mkRecordProp columnName dataType domainName isNullable)
+  pure $ Module ("Db" <> tableName) [ TypeDecl ("Db" <> tableName) (Record props) ]
+  where
+    emptyPos = Position { line: 0, column: 0 }
+    mkRecordProp :: String -> String -> Maybe String -> String -> RecordProp
+    mkRecordProp columnName dataType domainName isNullable =
+      let baseType = maybe
+                      (Primitive (dbNameToPrimitive dataType))
+                      -- TODO Need proper external reference
+                      (Ref emptyPos <<< ("Domains." <> _))
+                      domainName
+          optioned =
+            if isNullable == "YES"
+              then Option baseType
+              else baseType
+      in RecordProp columnName optioned
+    -- TODO Support other types (date/time types in particular)
+    sql = """
+          select column_name, data_type, domain_name, is_nullable
+          from information_schema.columns
+          where table_name = $1 and
+                  data_type in ('numeric', 'character varying', 'integer', 'smallint', 'text', 'boolean')
+          order by ordinal_position ;
+          """
 
 dbNameToPrimitive :: String -> Primitive
 dbNameToPrimitive = case _ of
@@ -38,11 +77,3 @@ dbNameToPrimitive = case _ of
   "text" -> PString
   "boolean" -> PBoolean
   _ -> PString -- XXX
-
-domainsSql :: String
-domainsSql = """
-  select domain_name, data_type
-  from information_schema.domains
-  where domain_schema = 'public' and
-          data_type in ('numeric', 'character varying', 'integer', 'smallint', 'text', 'boolean')
-  """
