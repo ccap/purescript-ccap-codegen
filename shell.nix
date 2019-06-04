4fee3a7c1602eff88c41d86244f639d69ce1ed7e
Ivan Jager <aij+git@mrph.org>
2019-06-04 17:39:56 -0500

    wip Upgrade purescript

--- shell.nix
+++ shell.nix
@@ -6,15 +6,19 @@ let
     repo = "easy-purescript-nix";
 
     # at this git revision:
-    rev = "828e34277dfd77507324d47f3a5e84afa81183c4";
+    rev = "80e6c66c7b5cffefeb823ebebf156d2d7acd3285";
 
     # this sha can be obtained by using
     # nix-prefetch-git https://github.com/justinwoo/easy-purescript-nix --rev 828e34277dfd77507324d47f3a5e84afa81183c4
     # (without --rev for latest master)
-    sha256 = "1ig0ayqdvbww7f2dvd3dflsmgc16r7df7k0iysb2rmy6ykf3balq";
+    sha256 = "0npvnr3h4vnq6ibwi9gvxgijyjnwmmyvqglq471wkkn6b4ygry9v";
   });
 in pkgs.stdenv.mkDerivation {
   name = "purescript-ccap-codegen";
   src = ./.;
-  buildInputs = easy-ps.buildInputs;
+  buildInputs = with pkgs; with easy-ps; [
+    purs
+    spago
+    nodejs-10_x
+  ];
 }
