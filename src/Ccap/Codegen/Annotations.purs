015ba40a30b046c96ebb507c9b7a066cb4e36d52
Ivan Jager <aij+git@mrph.org>
2019-06-05 14:16:21 -0500

    Post-merge cleanup

--- src/Ccap/Codegen/Annotations.purs
+++ src/Ccap/Codegen/Annotations.purs
@@ -6,26 +6,17 @@ import Prelude
 import Ccap.Codegen.Types (Annotation(..), Annotations, AnnotationParam(..))
 import Control.Alt ((<|>))
 import Data.Array as Array
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 
-arrayMap :: forall a k v. Eq k => (a -> k) -> (a -> v) -> Array a -> k -> Maybe v
-arrayMap k v arr i =
-  Array.findMap (\e -> if k e == i then Just $ v e else Nothing) arr
-
-lookupAnnotation :: String -> Annotations -> Maybe (Array AnnotationParam)
-lookupAnnotation =
-  arrayMap (\(Annotation l _ _) -> l) (\(Annotation _ _ p) -> p) # flip
-
-lookupParam :: String -> Array AnnotationParam -> Maybe (Maybe String)
-lookupParam =
-  arrayMap (\(AnnotationParam k _ _) -> k) (\(AnnotationParam _ _ v) -> v) # flip
-
-field :: String -> String -> Annotations -> Maybe (Maybe String)
-field s f = lookupAnnotation s >=> lookupParam f
+field :: String -> String -> Annotations -> Maybe String
+field annotKey paramKey annots = do
+  Annotation _ _ params <- Array.find (\(Annotation n _ _) -> n == annotKey) annots
+  AnnotationParam _ _ v <- Array.find (\(AnnotationParam n _ _) -> n == paramKey) params
+  v
 
 getWrapOpts :: String -> Annotations -> Maybe { typ :: String, wrap :: String, unwrap :: String }
 getWrapOpts lang an =
-  let f n = field lang n an # join
+  let f n = field lang n an
   in do
     typ <- f "t"
     wrap <- f "wrap" <|> pure ""
