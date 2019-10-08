1f64811f76204916aaae54fb61ea3e202dceec78
Philip Wales <Philip.Wales@wicourts.gov>
2019-10-08 10:25:58 -0500

    Generated module is specified by it's exports
    Module name from the file name is only relevant for tmpl file imports

---
+++ src/Ccap/Codegen/Parser/Export.purs
@@ -0,0 +1,23 @@
+module Ccap.Codegen.Parser.Export
+  where
+
+import Prelude
+
+import Data.Foldable (class Foldable, intercalate)
+import Data.String (Replacement(..))
+import Data.String as String
+import Data.String.Pattern (Pattern(..))
+import Node.Path (FilePath)
+import Node.Path as Path
+
+sep :: String
+sep = "."
+
+split :: String -> Array String
+split = String.split (Pattern sep)
+
+join :: forall f. Foldable f => f String -> String
+join = intercalate sep
+
+toPath :: String -> FilePath
+toPath = String.replaceAll (Pattern sep) (Replacement Path.sep)
